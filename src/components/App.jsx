61655da4e3bad495847ca8bc9060828bbb8c0011
PawelPalasinski <pawel.palasinski@gmail.com>
2022-08-19 01:34:52 +0200

    all file names amended

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { FeedbackOptionsV2 } from './FeedbackOptionsV2/FeedbackOptionsV2';
 export const App = () => {
   return (
     <div>
-      <FeedbackOptionsV1 />
+      <FeedbackOptionsV2 />
     </div>
   );
 };
